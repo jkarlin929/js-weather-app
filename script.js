b01fa0ed3ef877207a43880f0abbd5aaf2430208
jkarlin929 <osida11572@gmail.com>
2019-04-14 18:13:56 -0400

    add font

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const form = document.querySelector('#form');
 const button = document.querySelector('#button');
 //selects the zipcode value from the input
 const zipcode = document.querySelector('#zip');
-const weatherDiv = document.createElement('div');
+const weatherDiv = document.createElement('main');
 
 //get weather data through axios call
 const getData = async (zip) => {
@@ -16,6 +16,7 @@ const getData = async (zip) => {
   return resp.data;
 };
 
+//render weather data
 const renderWeather = data => {
   const weatherDesc = data.weather[0].description;
   const capWeatherDesc = weatherDesc.charAt(0).toUpperCase() + weatherDesc.slice(1);
@@ -29,19 +30,19 @@ const renderWeather = data => {
   //format sunet and sunrise times to
   const sunset = moment(sunsetTzOffset).format("h:mm a");
   const sunrise = moment(sunriseTzOffset).format("h:mm a");
-  console.log({sunset});
+  //render template string and append to body
   const weatherText = `
   <div class="weather">
-    <span>Location:</span> <span>${data.name}</span>
-    <span>Current Temp:</span> <span>${parseInt(data.main.temp)}°F</span>
-    <span>Overcast:</span> <span>${data.clouds.all}%</span>
-    <span>Humidity:</span> <span>${data.main.humidity}%</span>
-    <span>Pressure:</span> <span>${data.main.pressure} millibars</span>
-    <p>High Temp <i class="fas fa-temperature-high"></i></p> <p>${parseInt(data.main.temp_max)}°F</p>
-    <p>Low Temp <i class="fas fa-temperature-low"></i></p> <p>${parseInt(data.main.temp_min)}°F</p>
-    <p>Description:</p> <p>${capWeatherDesc}</p>
-    <p>Sunrise <i class="fas fa-sun"></i></p> <p>${sunrise}</p>
-    <p>Sunset <i class="far fa-sun"></i></p> <p>${sunset}</p>
+    <span>Location: ${data.name}</span>
+    <span>Current Temp: ${parseInt(data.main.temp)}°F</span>
+    <span>Overcast: ${data.clouds.all}%</span>
+    <span>Humidity: ${data.main.humidity}%</span>
+    <span>Pressure: ${data.main.pressure} millibars</span>
+    <p>Description: ${capWeatherDesc}</p>
+    <p><i class="fas fa-temperature-high"></i>  High Temp: ${parseInt(data.main.temp_max)}°F</p>
+    <p><i class="fas fa-temperature-low"></i>  Low Temp: ${parseInt(data.main.temp_min)}°F</p>
+    <p><i class="fas fa-sun"></i>  Sunrise: ${sunrise}</p>
+    <p><i class="far fa-sun"></i>  Sunset: ${sunset}</p>
   </div>
   `
   weatherDiv.innerHTML = weatherText;
