90fb1173c3b668f6b2f2d2e86dd8b92e987b404e
jkarlin929 <osida11572@gmail.com>
2019-04-14 14:24:37 -0400

    add data to body element

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const BASE_URL = 'http://api.openweathermap.org/data/2.5/weather?';
-const API_KEY = document.querySelector('[name="appid"]');
+const API_KEY = document.querySelector('[name="appid"]').value;
+console.log(API_KEY);
 
 //selects the form
 const form = document.querySelector('#form');
@@ -7,18 +8,30 @@ const form = document.querySelector('#form');
 const button = document.querySelector('#button');
 //selects the zipcode value from the input
 const zipcode = document.querySelector('#zip');
+const weatherDiv = document.createElement('div');
+
+
+//get weather data through axios call
+const getData = async (zip) => {
+  const resp = await axios(`${BASE_URL}zip=${zip}&units=imperial&appid=${API_KEY}`)
+  console.log(resp.data);
+  return resp.data;
+};
+
+const renderWeather = data => {
+  const weatherText = `
+  Name: <p>${data.name}</p>
+
+  `
+  weatherDiv.innerHTML = weatherText;
+  document.body.appendChild(weatherDiv);
+}
 
 //add event listener for the button in the form
-button.addEventListener('click', (e) => {
+button.addEventListener('click', async (e) => {
   e.preventDefault();
   const input = zipcode.value;
   console.log(input);
+  const weatherData = await getData(input);
+  renderWeather(weatherData);
 });
-
-
-
-// const getData = async (zip) => {
-//   const resp = await axios(`${BASE_URL}zip=${zip}&units=imperial&appid=${API_KEY}`)
-//   console.log(resp);
-//   return resp;
-// };
